e9dd08ee8f7dc09362dfaf1237f2585b0ddde5b0
Andrei Iurchenko <iur4.andrej@yandex.ru>
2023-06-21 13:48:20 +0300

    finished

---
+++ middlewares/auth.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errros/UnauthorizedError');
+
+const extractBearerToken = (header) => header.replace('Bearer ', '');
+
+module.exports = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+      next(new UnauthorizedError('You need to log in'));
+      return;
+    }
+
+    const token = extractBearerToken(authorization);
+    const payload = jwt.verify(token, 'gen');
+
+    req.user = payload;
+  } catch (err) {
+    next(new UnauthorizedError('You need to log in'));
+    return;
+  }
+  next();
+};
